6fef54e11761603607136748155bcfa94d4f5487
ChunKinChau <z5440431@student.unsw.edu.au>
2025-08-23 04:14:02 +1000

    add function add and edit for player predict

--- Main/python/database.py
+++ Main/python/database.py
@@ -8,6 +8,7 @@ url = os.environ.get("SUPABASE_URL")
 key = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
+#table Driver and Team 
 def addDriver(name: str, driver_Num: int, team: str):
     resp = supabase.table("Driver").insert({
         "name": name,
@@ -38,8 +39,43 @@ def addPoint(driverName: str, pointsAdd: int):
     row = res.data[0]
     new_points = (row.get("points") or 0) + pointsAdd
     upd = supabase.table("Driver").update({"points": new_points}).eq("name", row["name"]).execute()
+
+def subPoint(driverName: str, pointsAdd: int):
+    res = supabase.table("Driver").select("name, points").eq("name", driverName).limit(1).execute()
+
+    row = res.data[0]
+    new_points = (row.get("points") or 0) - pointsAdd
+    upd = supabase.table("Driver").update({"points": new_points}).eq("name", row["name"]).execute()
+
+#table PlayerResult
+def addPlayerPredict(playerName: str, raceCode: str, p1: int, p2: int, p3: int, p4: int, p5: int, p6: int, p7: int, p8: int, p9: int, p10: int, spec: int):
+    check = supabase.table("Player_predict").select("player_name").eq("player_name", playerName).eq("RaceCode", raceCode).execute()
+    lenCheck = len(check.data)
+    if(lenCheck <1):
+        resp = supabase.table("Player_predict").insert({"player_name": playerName, "RaceCode": raceCode,
+                                                        "P1": p1,
+                                                        "P2": p2,
+                                                        "P3": p3,
+                                                        "P4": p4,
+                                                        "P5": p5,
+                                                        "P6": p6,
+                                                        "P7": p7,
+                                                        "P8": p8,
+                                                        "P9": p9,
+                                                        "P10": p10,
+                                                        "special": spec
+                                                        }).execute()
+        print("Player predict:", resp.data)
+    else:
+        print("error")
     
+def editPlayerPredict(playerName: str, raceCode: str, column: str, value: int):
+    check = supabase.table("Player_predict").update({column: value}).eq("player_name", playerName).eq("RaceCode", raceCode).execute()
+    print("eddit:", column, value)
+
+
+
 # Run
-addPoint("Yuki", 20)
+
 
 
